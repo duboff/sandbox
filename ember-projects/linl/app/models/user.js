86a093a3f3cb7f945829ae906b48e2f463073ca0
Kyle E. Aungst <k.e.aungst@gmail.com>
2014-09-24 21:57:28 -0400

    incorporate new schema into user model Ember

--- ember-projects/linl/app/models/user.js
+++ ember-projects/linl/app/models/user.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 var User = DS.Model.extend({
-  partner: DS.hasMany('partner'),
+  partner: DS.belongsTo('partner'),
   children: DS.hasMany('child'),
   residences: DS.hasMany('residence'),
   first_name: DS.attr('string'),
@@ -9,14 +9,80 @@ var User = DS.Model.extend({
   password: DS.attr('string'),
   password_confirmation: DS.attr('string'),
   birthdate: DS.attr('date'),
-  marital_status: DS.attr('boolean'),
   state: DS.attr('string'),
-  ss_currently_collecting: DS.attr('boolean'),
-  ss_monthly_benefits_cents: DS.attr('number'),
   current_earnings_cents: DS.attr('number'),
   final_year_earnings_cents: DS.attr('number'),
   created_at: DS.attr('date'),
-  updated_at: DS.attr('date')
+  updated_at: DS.attr('date'),
+
+  ownerships: DS.hasMany('ownership'),
+
+  reserve_funds: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('asset').filterBy('type', 'reserve');
+    });
+  }.property('ownerships.@each.asset'),
+
+  regular_assets: function() {
+    return this.get('ownerships').then(function(ownerships){
+      return ownerships.mapBy('asset').filterBy('type', 'regular');
+    });
+  }.property('ownerships.@each.asset'),
+
+  other_incomes: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('asset').filterBy('other_income');
+    });
+  }.property('ownerships.@each.asset'),
+
+  special_expenses: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('assumption').filterBy('type', 'special');
+    });
+  }.property('ownerships.@each.assumption'),
+
+  tax_hikes: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('assumption').filterBy('type', 'tax');
+    });
+  }.property('ownerships.@each.assumption'),
+
+  inflations: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('assumption').filterBy('type', 'inflation');
+    });
+  }.property('ownerships.@each.assumption'),
+
+  benefit_cuts: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('assumption').filterBy('type', 'cut');
+    });
+  }.property('ownerships.@each.assumption'),
+
+  pensions: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('retirement').filterBy('type', 'pension');
+    });
+  }.property('ownerships.@each.retirement'),
+
+  roths: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('retirement').filterBy('type', 'roth');
+    });
+  }.property('ownerships.@each.retirement'),
+
+  iras: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('retirement').filterBy('type', 'ira');
+    });
+  }.property('ownerships.@each.retirement'),
+
+  social_securities: function() {
+    return this.get('ownerships').then(function(ownerships) {
+      return ownerships.mapBy('retirement').filterBy('type', 'ss');
+    });
+  }.property('ownerships.@each.retirement')
+
 });
 
 export default User;
