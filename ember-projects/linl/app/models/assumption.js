6291c5eb795608081c0ee6fc99e1149bb0e1507b
Kyle E. Aungst <k.e.aungst@gmail.com>
2014-09-25 12:08:17 -0400

    add assumption model

--- ember-projects/linl/app/models/assumption.js
+++ ember-projects/linl/app/models/assumption.js
@@ -1,25 +1,26 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+export default DS.model.extend({
   name: DS.attr('string'),
   type: DS.attr('string'),
-  rate_of_return: DS.attr('number'),
-  initial_amount_cents: DS.attr('number'),
-  final_amount_cents: DS.attr('number'),
   start_date: DS.attr('date'),
   end_date: DS.attr('date'),
+  current_percentage: DS.attr('number'),
+  future_percentage: DS.attr('number'),
+  monthly_cents: DS.attr('number'),
 
   ownerships: DS.hasMany('ownership'),
 
-  users: function(){
+  users: function() {
     return this.get('ownerships').then(function(ownerships){
       return ownerships.mapBy('user');
     });
   }.property('ownerships.@each.user'),
 
-  partners: function(){
+  partners: function() {
     return this.get('ownerships').then(function(ownerships){
       return ownerships.mapBy('partner');
     });
   }.property('ownerships.@each.partner')
+
 });
