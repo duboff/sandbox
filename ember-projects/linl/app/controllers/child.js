1b5f04b3aa5b5a0b0b401b12b2ea67e6057ac3af
Kyle E. Aungst <k.e.aungst@gmail.com>
2014-09-25 12:32:16 -0400

    Adds child controller to ember app

---
+++ ember-projects/linl/app/controllers/child.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  actions: {
+    saveChanges: function() {
+      console.log(this.get('fields'));
+      this.get('model').save();
+    }
+  },
+
+  childVisible: false
+
+});
