314dbc099dfdc9d670a8e7c3b26e13b46852a311
Kyle E. Aungst <k.e.aungst@gmail.com>
2014-09-28 17:54:15 -0400

    Updates partner serializers

--- rails-projects/linl-api/app/serializers/partner_serializer.rb
+++ rails-projects/linl-api/app/serializers/partner_serializer.rb
@@ -1,5 +1,6 @@
 class PartnerSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :name, :birthdate, :marital_status, :state, :ss_currently_collecting,
-    :ss_monthly_benefits_cents, :current_earnings_cents, :final_year_earnings_cents,
+  attributes :id, :user_id, :name, :birthdate, :marital_status, 
+    :state, :ss_currently_collecting, :current_earnings_cents, 
+    :final_year_earnings_cents, :roth_first, :retirement_date,
     :created_at, :updated_at
 end
